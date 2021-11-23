f9c602468d563f7f843430478991d3ba47727dbc
BAEKJungHo <designjava@naver.com>
2021-11-23 19:10:05 +0900

    Sample - 소스 구조 샘플
    - 이름
      - 챕터번호
         - 문제이름(with papago)
             - 풀이방식1
             - 풀이방식2

---
+++ src/jungho/chapter3/change/Solution.java
@@ -0,0 +1,9 @@
+package jungho.chapter3.change;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // implementation
+    }
+
+}
---
+++ src/jungho/chapter3/thelawofnumbers/Solution.java
@@ -0,0 +1,9 @@
+package jungho.chapter3.thelawofnumbers;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // implementation
+    }
+
+}
