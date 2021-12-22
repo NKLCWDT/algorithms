17a68e5e1c423950ba4bd5139f99b487ec442e47
jola7373 <jola7373@naver.com>
2021-12-22 22:52:15 +0900

    볼링공 고르기

---
+++ src/jihye/thisiscodingtest/part03/choseBowlingBall/Solution.java
@@ -0,0 +1,28 @@
+package jihye.thisiscodingtest.part03.choseBowlingBall;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int weight[] = new int[N];
+
+        for (int i = 0; i < weight.length; i++) {
+            weight[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (weight[i] != weight[j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
+    }
+}
