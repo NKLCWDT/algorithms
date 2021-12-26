240700a0d00e8c5e475b0cde45577faffd339ba2
jola7373 <jola7373@naver.com>
2021-12-26 19:59:34 +0900

    전체적으로 정리

--- src/jihye/thisiscodingtest/part03/Q15_findCity/Solution.java
+++ src/jihye/thisiscodingtest/part03/Q15_findCity/Solution.java
@@ -10,53 +10,54 @@ public class Solution {
         int K = sc.nextInt();
         int X = sc.nextInt();
         //그래프의 정보를 담는다.
-        ArrayList<Integer> graph[] = new ArrayList[N+1];
+        ArrayList<Integer> graph[] = new ArrayList[N + 1];
 
         //그래프를 ArrayList로
-        for(int i=0; i < N+1; i++){
+        for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<Integer>();
         }
 
         //간선 정보를 담는다.
-        for(int i=0; i < M; i++){
+        for (int i = 0; i < M; i++) {
             graph[sc.nextInt()].add(sc.nextInt());
         }
 
-        int [] distance = bfs(graph, X);
+        int[] distance = bfs(graph, X);
 
         boolean check = false;
-        for(int i=0; i < distance.length;i++){
-            if(distance[i] == K){
+        for (int i = 0; i < distance.length; i++) {
+            if (distance[i] == K) {
                 System.out.println(i);
                 check = true;
             }
         }
-        if(check == false){
+        if (check == false) {
             System.out.println(-1);
         }
     }
-    public static int[] bfs(ArrayList[] graph, int start){
+
+    public static int[] bfs(ArrayList[] graph, int start) {
         //최단 거리를 담는다.
-        int distance[] = new int[graph.length +1];
+        int distance[] = new int[graph.length + 1];
 
         //처음에는 모든 거리를 -1로 초기화 한다.
-        Arrays.fill(distance,-1);
+        Arrays.fill(distance, -1);
         //출발지점은 0으로 설정한다.
-        distance[start] =0;
+        distance[start] = 0;
         Queue<Integer> queue = new LinkedList<>();
 
         //시작 노드를 큐에 담는다.
         queue.add(start);
         //큐가 빌때까지
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             //큐에서 꺼낸다.
             int now = queue.poll();
             int count = 0;
             //큐에서 꺼낸 노드의 연결된 노드들까지 거리 담는다.
             while (graph[now].size() > count) {
                 int next = (int) graph[now].get(count);
-                if(distance[next]==-1){
-                    distance[next] = distance[now]+1;
+                if (distance[next] == -1) {
+                    distance[next] = distance[now] + 1;
                     queue.add(next);
                 }
                 count++;
--- src/jihye/thisiscodingtest/part03/Q37_Floid/Solution.java
+++ src/jihye/thisiscodingtest/part03/Q37_Floid/Solution.java
@@ -15,11 +15,11 @@ public class Solution {
         m = sc.nextInt();
         graph = new int[n + 1][n + 1];
 
-        for(int i=0; i < n+1; i++){
+        for (int i = 0; i < n + 1; i++) {
             Arrays.fill(graph[i], INF);
         }
 
-        for(int i=1; i< n+1; i++){
+        for (int i = 1; i < n + 1; i++) {
             graph[i][i] = 0;
         }
 
