193bf8175507d813ae0d1e46b12963a3f7468d94
jola7373 <jola7373@naver.com>
2022-01-02 21:36:56 +0900

    곱하기 혹은 더하기

--- src/jihye/thisiscodingtest/part03/populationMovement/Solution.java
+++ src/jihye/thisiscodingtest/part03/populationMovement/Solution.java
@@ -3,15 +3,22 @@ package jihye.thisiscodingtest.part03.populationMovement;
 import java.util.*;
 
 public class Solution {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static int N;
+    static int R;
+    static int graph[][] = new int[N][N];
+    static int[][] union = new int[N][N];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        N = sc.nextInt();
         int L = sc.nextInt();
-        int R = sc.nextInt();
-        int graph[][] = new int[N][N];
+        R = sc.nextInt();
 
-        for(int i=0; i < N; i++){
-            for(int j=0; j < N; j++){
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 graph[i][j] = sc.nextInt();
             }
         }
@@ -29,26 +36,59 @@ public class Solution {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     if (union[i][j] == -1) {
-                        process(i,j,index);
-                        index +=1;
-                    }if(index == N*N){
+                        process(i, j, index);
+                        index += 1;
+                    }
+                    if (index == N * N) {
                         break;
-                    }totalCount +=1;
+                    }
+                    totalCount += 1;
                 }
-            }
+            }System.out.println(totalCount);
         }
-        System.out.println(totalCount);
+
 
     }
-    public process(int x, int y, int index){
+
+    public static int process(int x, int y, int index) {
         ArrayList<int[]> united = new ArrayList<>();
-        int[] info = {x,y};
+        int[] info = {x, y};
         united.add(info);
 
         Queue queue = new LinkedList();
         queue.add(info);
-        int [][] union ;
         union[x][y] = index;
-        summary
+        int summary = graph[x][y];
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            int[] direction = (int[]) queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (0 <= nx && nx < N && 0 <= ny && ny < N && union[nx][ny] == -1) {
+                    int calc = Math.abs(graph[nx][ny] - graph[x][y]);
+                    if (1 <= calc && calc <= R){
+                        info = new int[2];
+                        info[0] = nx;
+                        info[1] = ny;
+                        queue.add(info);
+
+                        union[nx][ny] = index;
+                        summary += graph[nx][ny];
+                        count++;
+                        united.add(info);
+                    }
+                }
+            }
+        }
+        for(int i=0; i< N;i++){
+            for(int j=0; j < N; j++){
+                graph[i][j] = summary/count;
+            }
+        }
+        return count;
+
     }
 }
