2d4ed82c16cad47eeee5004889280019aee03152
BAEKJungHo <designjava@naver.com>
2022-01-13 21:38:32 +0900

    피드백 반영
    - concat 네이밍 append 로 변경
    - 삼항 연산자 대신 Math.min 사용 -> 인텔리제이가 추천함

--- src/jungho/thisiscodingtest/part03/Q09_compressstring/Solution.java
+++ src/jungho/thisiscodingtest/part03/Q09_compressstring/Solution.java
@@ -39,20 +39,20 @@ public class Solution {
 
             // i 개 단위로 잘린 현재 문자열(current) 와 비교하기 위한 next 문자열 구하는 반복문
             for (int k = i; k < alphabet.length(); k = k + i) {
-                int nextLastIndex = k + i < alphabet.length() ? k + i : alphabet.length();
+                int nextLastIndex = Math.min(k + i, alphabet.length());
                 String next = alphabet.substring(k, nextLastIndex);
 
                 if (current.equals(next)) {
                     count++;
                 } else {
-                    concatToStringBuilder(stringBuilder, count, current);
+                    appendToStringBuilder(stringBuilder, count, current);
                     count = 1;
                     current = next;
                 }
             }
             // Ex. alphabet 길이가 16이고 3개 단위로 자른다고 했을때, 내부 반복문의 k 값은 15 까지만 가능함.
             // 따라서 내부 반복문 끝나면 한 번더 남은 문자열을 붙여줘야 함
-            concatToStringBuilder(stringBuilder, count, current);
+            appendToStringBuilder(stringBuilder, count, current);
 
             answer = Math.min(answer, stringBuilder.length());
         }
@@ -60,7 +60,7 @@ public class Solution {
         return answer;
     }
 
-    static void concatToStringBuilder(StringBuilder stringBuilder, int count, String str) {
+    static void appendToStringBuilder(StringBuilder stringBuilder, int count, String str) {
         if(count > 1) {
             stringBuilder.append(count);
         }
