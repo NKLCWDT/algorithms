6b305976179728461dc9e65a6b9e0e090430c33d
ann-mj <87982338+ann-mj@users.noreply.github.com>
2022-01-13 20:33:50 +0900

    메소드 명은 소문자로 시작

--- src/annmj/thisiscodingtest/part03/Q34_string_compress/Main.java
+++ src/annmj/thisiscodingtest/part03/Q34_string_compress/Main.java
@@ -34,7 +34,7 @@ class Solution {
                  * 이 때는 i + unit 부터 탐색을 해야한다.
                  */
                 for (int j = i + unit; j < s.length(); j++) {
-                    if (NotEqualToPrev(prev, j, s.charAt(j), unit)) {
+                    if (notEqualToPrev(prev, j, s.charAt(j), unit)) {
                         break;
                     }
 
@@ -56,7 +56,7 @@ class Solution {
         return answer;
     }
 
-    private boolean NotEqualToPrev(StringBuilder prev, int j, char c, int unit) {
+    private boolean notEqualToPrev(StringBuilder prev, int j, char c, int unit) {
         return c != prev.toString().charAt(j % unit);
     }
 }
