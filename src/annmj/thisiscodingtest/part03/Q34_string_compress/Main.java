4afc33955cafb9172d83a149b8b00cb4af2b919b
ann-mj <87982338+ann-mj@users.noreply.github.com>
2022-01-13 19:58:56 +0900

    length 변수명 변경, 반복문에 주석 추가, 불필요한 if 문제거

--- src/annmj/thisiscodingtest/part03/Q34_string_compress/Main.java
+++ src/annmj/thisiscodingtest/part03/Q34_string_compress/Main.java
@@ -6,17 +6,33 @@ class Solution {
 
     public int solution(String s) {
         int maxUnit = s.length();
-        int length = MAX_LENGTH; // 가장 짧은 것의 길이
+        int answer = MAX_LENGTH;
         int unit = 1; // 문자열을 나누는 단위
+
+        /* unit 이 최대길이(문자열의 길이)가 될 때까지 반복 */
         while (unit <= maxUnit) {
             String result = ""; // 문자열 압축 결과
             for (int i = 0; i < s.length(); ) {
                 StringBuilder prev = new StringBuilder();
-                int count = 1; // 같은 묶음의 개수
+                int count = 1;            /* 같은 묶음의 개수, prev 와 같은 묶음의 개수 */
+
+                /*
+                * ababcd 문자열에서 unit 이 2인 경우,
+                * prev 에는 ab 가 저장되고, 이후에 다시 반복문이 실행되면 ab, cd 순으로 저장되게 될 겁니다.
+                *
+                * ababcd 문자열에서 unit 이 5인 경우,
+                * prev 에는 ababc 가 저장
+                *
+                * i + unit 이 s.length 넘어가면 안되므로 for 문의 조건에 추가
+                * */
                 for (int j = i; j < i + unit && j < s.length(); j++) {
                     prev.append(s.charAt(j));
                 }
 
+                /*
+                 * prev 를 뽑았다면, prev 와 같은 문자를 압축해야 하는데,
+                 * 이 때는 i + unit 부터 탐색을 해야한다.
+                 */
                 for (int j = i + unit; j < s.length(); j++) {
                     if (s.charAt(j) != prev.toString().charAt(j % unit)) {
                         break;
@@ -26,18 +42,17 @@ class Solution {
                         count++;
                     }
                 }
-                i = i + count * unit;
-                if (count != 1) {
-                    result += String.valueOf(count) + prev;
-                }
+                i = i + (count * unit);
 
-                if (count == 1) {
-                    result += prev;
+                /* count 가 1이 아닌 경우 (ex. 2) 에만 result 에 더해준다. */
+                if (count != 1) {
+                    result += String.valueOf(count);
                 }
+                result += prev;
             }
-            length = Math.min(result.length(), length);
+            answer = Math.min(result.length(), answer);
             unit++;
         }
-        return length;
+        return answer;
     }
 }
