77b0bbb7ae08b71b2300e49f509a4c2348184d07
jola7373 <jola7373@naver.com>
2022-01-18 19:07:57 +0900

    블록 이동하기 푸는중

--- src/jihye/thisiscodingtest/part03/Q09_stringCompression/Solution.java
+++ src/jihye/thisiscodingtest/part03/Q09_stringCompression/Solution.java
@@ -1,7 +1,6 @@
 package jihye.thisiscodingtest.part03.Q09_stringCompression;
 
 public class Solution {
-    private static boolean check = false;
 
     public static void main(String[] args) {
         String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
@@ -13,53 +12,51 @@ public class Solution {
         int answer = s.length();
 
         while (true) {
-            if (unit > s.length() / 2) {//자르는 단위가 절반을 넘어가면 break
+            if (unit > s.length() / 2) {    //자르는 단위가 절반을 넘어가면 break
                 break;
             } else {
-                int n = CompressedStringLength(unit, s);
-                if (check == true) {
-                    answer = Math.min(answer, n);
-                }
+                int n = getCompressedStringLength(unit, s);
+                answer = Math.min(answer, n);
             }
             unit++;
-            check = false;
         }
 
         return answer;
     }
 
-    public static int CompressedStringLength(int unit, String s) {//문자열 찾기 구간
+    public static int getCompressedStringLength(int unit, String s) {   //문자열 찾기 구간
         StringBuilder compressedString = new StringBuilder();
-        String oneUnit = s.substring(0, unit);//하나의 일치하는 스트링 단위
+        String current = s.substring(0, unit);  //하나의 일치하는 스트링 단위
         int end = 0;
+
         //문자열을 자르는 단위만큼 잘라서 다음 단위와 일치하는지
         //일치한다면 그 수를 count해서 newString에 넣어준다
+
         for (int i = unit; i < s.length(); i += unit) {
             int count = 1;
 
-            while (isSame(i , s, unit, oneUnit)) {
+            while (isSame(i , s, unit, current)) {
                 count++;
                 i = i + unit;
             }
-            if (count > 1) {//count가 2이상일때
-                check = true;
-                compressedString.append(Integer.toString(count));//newString에 count와 반복되는 string(one)을 넣는다                System.out.println(newString);
-                compressedString.append(oneUnit);
-            } else {
-                compressedString.append(oneUnit);
+            if (count > 1) {    //count가 2이상일때
+                compressedString.append(Integer.toString(count));   //newString에 count와 반복되는 string(one)을 넣는다                System.out.println(newString);
             }
-            if (i + unit > s.length()) {//범위 체크
+            compressedString.append(current);
+
+            if (i + unit > s.length()) {    //범위 체크
                 end = i;
                 break;
             }
-            oneUnit = s.substring(i, i + unit);
-
+            current = s.substring(i, i + unit);
         }
-        if (end <= s.length()) {//만일 남은 unit단위 보다 작은 문자열이 남았을시 더해준다.
+
+        if (end <= s.length()) {    //만일 남은 unit단위 보다 작은 문자열이 남았을시 더해준다.
             compressedString.append(s.substring(end));
         }
         return compressedString.length();
     }
+
     public static boolean isSame(int i , String s, int unit, String oneUnit){
         return i <= s.length() - unit && s.substring(i, i + unit).equals(oneUnit);
     }
